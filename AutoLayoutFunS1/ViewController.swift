eb29591dda45f30d6043ed1a7a8e87252970ada4
Gina Sprint <sprint@gonzaga.edu>
2020-10-07 10:56:56 -0700

    end of wednesday's class

--- AutoLayoutFunS1/ViewController.swift
+++ AutoLayoutFunS1/ViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+// MARK: - Auto Layout
+// so far, we have been hardcoding the positioning of our views
+// a view is defined by a rectangle
+// this rectangle has lots of properties (attributes)
+// origin: the upper left hand corner and its used to position the view in its super view (container view)
+// size: the width and the height of the view
+// point: is a relative unit of measurment
+// the number of pixels in a point is dependent on the screen
+// demo 1:
+// auto layout will determine the size and position and runtime based on the size of the screen
+// auto layout requires that every view has at **least 4 constraints**
+// constraint: a relationship between views that can be determined at runtime
+// 4 contraints
+// position: x and y constraint (horizontal and vertical)
+// size: width and height constraint
+// views have implicit width and height based on their content
+// makes our job easier because we can use the implicit width and height constraints
+// demo 2: 
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
