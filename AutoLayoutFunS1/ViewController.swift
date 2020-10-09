85a726ce92d4a0587be7bdb6e6d0a83bf0465598
Gina Sprint <sprint@gonzaga.edu>
2020-10-09 10:19:24 -0700

    two button demo

--- AutoLayoutFunS1/ViewController.swift
+++ AutoLayoutFunS1/ViewController.swift
@@ -26,7 +26,7 @@ import UIKit
 // views have implicit width and height based on their content
 // makes our job easier because we can use the implicit width and height constraints
 // demo 2: add alignment constraints for the button to make it centered horizontally and vertically in its container
-// (what is a container view? a view the contains other views)
+// (what is a container view? a view that contains other views)
 
 class ViewController: UIViewController {
 
